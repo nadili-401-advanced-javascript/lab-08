c57f104d56ff5f3928926877849d47490ccf4c65
Nadya Ilinskaya <nadya.ilinskiy@gmail.com>
2019-10-25 23:23:47 -0700

    update tests

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -8,14 +8,14 @@ const mockRequest = supertester(server);
 // this is actually server.js > server
 // (akin to server.start, we're doing server.server)
 
-// describe('web server', () => {
-//   it('should respond properly on request to /people', () => {
-//     mockRequest
-//       .get('/people')
-//       .then(results => {
-//         expect(results.status).toBe(200);
-//         expect(results.body.count).toBe(4);
-//       })
-//       .catch(console.error);
-//   });
-// });
+describe('web server', () => {
+  it('should respond properly on request to /people', () => {
+    mockRequest
+      .get('/people')
+      .then(results => {
+        expect(results.status).toBe(200);
+        expect(results.body.count).toBe(4);
+      })
+      .catch(console.error);
+  });
+});
